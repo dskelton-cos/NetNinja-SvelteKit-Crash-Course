33413fd412aa58845883457366413ba102f054dd
David Skelton <dskelton@cityofsydney.nsw.gov.au>
2022-01-21 15:10:49 +1100

    Tut 3. working on mac

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let title = "Ninja gaming";
+
+  const updateTitle = () => {
+    title = "New Ninja gaming";
+  };
+</script>
+
+<div class="index">
+  <h1>{title}</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+  <button on:click={updateTitle}> Update</button>
+  <input type="text" bind:value={title} >
+</div>
+
+<style>
+    .index {
+        margin: 3rem auto;
+        display: block;
+        color: brown;
+    }
+</style>
